4ac41e904372247750ddcfcda20ea1aed9bb9c86
jdwhitneyhumphrey <124301860+jdwhitneyhumphrey@users.noreply.github.com>
2023-05-21 21:39:51 -0700

    Update Sample.cs

--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -1,17 +1,27 @@
 using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class Sample : MonoBehaviour
 {
+    public GameManager gameManager;
+    private FirstPersonController fpsController;
+    public GameObject player;
     public bool IsSolved = false;
 
-    void OnTriggerEnter (Collider collider)
+    void OnTriggerEnter(Collider other)
     {
-        GameObject collidedWith = collider.gameObject;
+       fpsController = player.GetComponent<FirstPersonController>();
+        IsSolved = true;
+    }
+    void OnGUI()
 
-        if (collidedWith.tag == gameObject.tag)
+    {
+        if (IsSolved == true)
         {
-            IsSolved = true;
-            Destroy(collidedWith);
+            Rect rect = new Rect(Screen.width / 2 - 80, Screen.height / 2 - 100, 120, 50);
+            GUI.Box(rect, "Sample Collected");
         }
+
     }
 }
