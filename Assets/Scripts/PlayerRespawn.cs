98753be4b842034fd3f576edb198b0f1f17affca
jdwhitneyhumphrey <124301860+jdwhitneyhumphrey@users.noreply.github.com>
2023-05-21 19:34:14 -0700

    Update PlayerRespawn.cs

--- Assets/Scripts/PlayerRespawn.cs
+++ Assets/Scripts/PlayerRespawn.cs
@@ -7,7 +7,7 @@ public class PlayerRespawn : MonoBehaviour
 	public GameManager gameManager; 
 
 	// Triggers when the player enters the water
-	void Start
+	void OnTriggerEnter(Collider other)
 	{
 		// Moves the player to the spawn point
 		gameManager.PositionPlayer();
